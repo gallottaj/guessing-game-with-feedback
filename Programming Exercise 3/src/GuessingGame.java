183607ab986d51969588244f23835d4153b41ce1
Joe Gallotta <joeygallotta@gmail.com>
2019-12-07 10:30:44 -0800

    test user input

---
+++ Programming Exercise 3/src/GuessingGame.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class GuessingGame {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter an integer between 1 and 10000:");
+		int input = in.nextInt();
+		System.out.print(input);
+	}
+}
--- Programming Exercise 3/src/guessing_game.java
+++
@@ -1,4 +0,0 @@
-
-public class guessing_game {
-
-}
