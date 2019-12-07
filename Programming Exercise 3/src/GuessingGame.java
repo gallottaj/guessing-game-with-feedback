e6919721533c7f2168dfc68524ecbd9a28fe11cb
Joe Gallotta <joeygallotta@gmail.com>
2019-12-07 10:38:18 -0800

    test random integer generator

--- Programming Exercise 3/src/GuessingGame.java
+++ Programming Exercise 3/src/GuessingGame.java
@@ -6,6 +6,12 @@ public class GuessingGame {
 		Scanner in = new Scanner(System.in);
 		System.out.print("Enter an integer between 1 and 10000:");
 		int input = in.nextInt();
-		System.out.print(input);
+		System.out.println("Integer between 1 and 10000 = " + getRandomIntegerBetweenRange(1, 10000));
+
+	}
+
+	public static double getRandomIntegerBetweenRange(double min, double max) {
+		double random_number = (int) (Math.random() * ((max - min) + 1)) + min;
+		return random_number;
 	}
 }
