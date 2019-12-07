8184f2e9afdca17e0b630a605cf4b78b1140dbcb
Joe Gallotta <joeygallotta@gmail.com>
2019-12-07 12:45:58 -0800

    add if too high and if too low statements

--- Programming Exercise 3/src/GuessingGame.java
+++ Programming Exercise 3/src/GuessingGame.java
@@ -1,17 +1,35 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class GuessingGame {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Random r = new Random();
+		int random_number = r.nextInt(10000); // random integer between 0 and 10000
 		Scanner in = new Scanner(System.in);
+		System.out.print(random_number);
 		System.out.print("Enter an integer between 1 and 10000:");
 		int input = in.nextInt();
-		System.out.println("Integer between 1 and 10000 = " + getRandomIntegerBetweenRange(1, 10000));
 
+		// You Win
+		if (input == random_number) {
+			System.out.print("you win!");
+		}
+
+		// Too high
+		if (input > random_number) {
+			int eligible_range_higher = random_number + 25;
+			int eligible_range_lower = random_number - 25;
+
+			System.out.print("Too high! Hint: Enter a number between" + " " + eligible_range_lower + " " + "and" + " "
+					+ eligible_range_higher);
+
+		}
+
+		// Too Low
+
+		if (input < random_number) {
+			System.out.print("try again");
+		}
 	}
 
-	public static double getRandomIntegerBetweenRange(double min, double max) {
-		double random_number = (int) (Math.random() * ((max - min) + 1)) + min;
-		return random_number;
-	}
 }
