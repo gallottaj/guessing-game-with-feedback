33e3926aee25f9a96906d676d0100372a10c8b1b
Joe Gallotta <joeygallotta@gmail.com>
2019-12-10 18:14:47 -0800

    final game with tests

--- Programming Exercise 3/src/GuessingGame.java
+++ Programming Exercise 3/src/GuessingGame.java
@@ -5,31 +5,24 @@ public class GuessingGame {
 	public static void main(String[] args) {
 		Random r = new Random();
 		int random_number = r.nextInt(10000); // random integer between 0 and 10000
-		Scanner in = new Scanner(System.in);
-		System.out.print(random_number);
-		System.out.print("Enter an integer between 1 and 10000:");
-		int input = in.nextInt();
+//		System.out.print(random_number + " " + ""); // to test the random integer
+		int user_guess = 0;
+		int eligible_range_higher = random_number + 10;
+		int eligible_range_lower = random_number - 10;
 
-		// You Win
-		if (input == random_number) {
-			System.out.print("you win!");
-		}
+		do {
+			System.out.print("Enter an integer between 1 and 10000:");
+			Scanner in = new Scanner(System.in);
+			user_guess = in.nextInt();
 
-		// Too high
-		if (input > random_number) {
-			int eligible_range_higher = random_number + 25;
-			int eligible_range_lower = random_number - 25;
-
-			System.out.print("Too high! Hint: Enter a number between" + " " + eligible_range_lower + " " + "and" + " "
-					+ eligible_range_higher);
-
-		}
-
-		// Too Low
-
-		if (input < random_number) {
-			System.out.print("try again");
-		}
+			if (user_guess > random_number) // too low
+				System.out.println("HIGHER!" + " " + "Try a number between" + " " + eligible_range_lower + " " + "and"
+						+ " " + eligible_range_higher);
+			else if (user_guess < random_number) // too high
+				System.out.println("LOWER!" + " " + "Try a number between" + " " + eligible_range_lower + " " + "and"
+						+ " " + eligible_range_higher);
+			else // winner
+				System.out.println("WINNER!");
+		} while (user_guess != random_number);
 	}
-
 }
